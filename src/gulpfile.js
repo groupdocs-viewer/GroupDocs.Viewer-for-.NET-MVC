0091f251a23e472691902f9f373ee3813077aa83
pavelteplitsky <pavel.teplitsky@aspose.com>
2019-05-30 14:08:46 +0300

    Updated gulp

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp')
 gulp.task('build', function () {
     return gulp
-        .src('./node_modules/@groupdocs.examples.angular/viewer/dist/**')
+        .src('./node_modules/@groupdocs.examples.angular/viewer/**')
         .pipe(gulp.dest('./Resources/'))
 })
