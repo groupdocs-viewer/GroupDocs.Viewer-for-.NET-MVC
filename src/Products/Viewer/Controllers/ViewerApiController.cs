336d0daa0246eab9ad89807cbc0af6524f6a92ff
Egor <egor.krushinov@aspose.com>
2020-08-04 10:39:46 +0300

    Fixed formatting.

--- src/Products/Viewer/Controllers/ViewerApiController.cs
+++ src/Products/Viewer/Controllers/ViewerApiController.cs
@@ -674,7 +674,10 @@ namespace GroupDocs.Viewer.MVC.Products.Viewer.Controllers
             // get/set parameters
             string documentGuid = postedData.guid;
             string password = string.IsNullOrEmpty(postedData.password) ? null : postedData.password;
-            if (!File.Exists(documentGuid)) { throw new Exception("File not found."); }
+            if (!File.Exists(documentGuid)) 
+            { 
+                throw new Exception("File not found."); 
+            }
 
             string cachePath;
             ViewerSettings settings = GetViewerSettings(documentGuid, out cachePath);
